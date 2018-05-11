8de4c48e51429c027d455d6ceff7c9b35f429c9e
Francesco Bianco <info.francescobianco@gmail.com>
2018-05-11 10:05:07 +0200

    deploy@

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,3 @@ DIST=master
 git config credential.helper cache && git commit -am "deploy@" && git push
 
 ssh $USER@$HOST "wget --no-check-certificate -O - https://rawgit.com/$REPO/$DIST/setup.sh | bash"
-wget --no-check-certificate -O - https://rawgit.com/francescobianco/srv-vpscheapnet-basic-milicio.tk/master/setup.sh  | bash
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Hello World!"
+
 ls
 
 date
