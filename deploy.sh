6973cbc5e9d4ca6fb4ec246cb61676191f73eb44
Francesco Bianco <info.francescobianco@gmail.com>
2018-05-11 10:11:59 +0200

    deploy@

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,4 @@ DIST=master
 
 git config credential.helper cache && git commit -am "deploy@" && git push
 
-ssh root@MachineB 'bash -s' < remote.sh
-
-ssh $USER@$HOST "wget -q --no-check-certificate -O - https://rawgit.com/$REPO/$DIST/setup.sh"
+ssh $USER@$HOST "wget -q --no-check-certificate -O - https://rawgit.com/$REPO/$DIST/setup.sh | bash"
